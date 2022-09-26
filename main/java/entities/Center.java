a03519afd83ebe5b264c8f790feb264d874c9ab0
Rayane Chaoub <rayanechaoub2@gmail.com>
2022-09-26 17:34:18 +0200

    premiere version backend

---
+++ main/java/entities/Center.java
@@ -0,0 +1,30 @@
+package entities;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import javax.persistence.*;
+import java.util.List;
+
+@FieldDefaults(level= AccessLevel.PRIVATE)
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Center {
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long    id;
+    String  name;
+    @ManyToOne
+    City    city;
+    @OneToMany
+    List<Doctor> centres;
+
+    public Center(String name, City city) {
+        this.name = name;
+        this.city = city;
+    }
+
+}
